b5a870fdcc2be5757578c8cfa61047236f5dc8b7
Miraya Abrodi <mirayaabrodi@gmail.com>
2023-05-22 07:25:34 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput placeholder="Yout goals!" />
+        <Button title="Add goal" />
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
@@ -13,8 +17,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 10,
+    borderWidth: 3,
+    borderColor: "red",
+    padding: 16,
   },
 });
